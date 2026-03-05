fd776fbfaf1f853873303a4c143ea7739d9dc944
Shukurulloh <shukurxondev@gmail.com>
2026-03-05 12:00:00 +0500

    Updated

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,52 @@
 const productModel = require("../models/product-model");
 
 class UserController {
+  async getProducts(req, res) {
+    try {
+      const { searchQuery, filter, category, page, pageSize } = req.query;
+
+      const skipAmount = (+page - 1) * +pageSize;
+
+      let query = {};
+
+      if (searchQuery) {
+        const escapedSearchQuery = searchQuery.replace(
+          /[.*+?^{}()|[\]\\]/g,
+          "\\$&",
+        );
+        query.$or = [{ name: { $regex: new RegExp(escapedSearchQuery, "i") } }];
+      }
+      if (category) {
+        const escapedSearchQuery = searchQuery.replace(
+          /[.*+?^{}()|[\]\\]/g,
+          "\\$&",
+        );
+        query.$or = [
+          { category: { $regex: new RegExp(escapedSearchQuery, "i") } },
+        ];
+      }
+      let sortOptions = { createdAt: -1 };
+      if (filter === "newest") {
+        sortOptions = { createdAt: -1 };
+      } else if (filter === "oldest") {
+        sortOptions = { createdAt: 1 };
+      }
+
+      const products = await productModel
+        .find(query)
+        .sort(sortOptions)
+        .skip(skipAmount)
+        .limit(+pageSize);
+
+      const totalProduct = await productModel.countDocuments(query);
+
+      const isNext = totalProduct > skipAmount + +products.length;
+
+      return res.json({ products, isNext, totalProduct });
+    } catch (err) {
+      console.log(err);
+    }
+  }
   async topProducts(req, res) {
     try {
       const topProducts = await productModel.find({ top: true, active: true });
--- route/user.js
+++ route/user.js
@@ -2,6 +2,7 @@ const userController = require("../controllers/user-controller");
 
 const router = require("express").Router();
 
+router.get("/products", userController.getProducts);
 router.get("/top-products", userController.topProducts);
 router.get("/product/:id", userController.getProduct);
 
